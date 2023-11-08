12810a54bf3429068b500e86f2828203cf23c4a4
Renato Zimerfeld <renato.zimerfeld@radixeng.com>
2023-11-08 10:15:04 -0300

    q

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
